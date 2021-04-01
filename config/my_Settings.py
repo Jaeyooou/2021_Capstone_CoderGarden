ed7890c1f6beaec6ddf096a49a4ea6d60a8f1592
jaeyooouinthebuildin <jaehyen2@naver.com>
2021-04-01 18:11:52 +0900

    init django

---
+++ config/my_Settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+# 나중에 ignore 에 추가하기
