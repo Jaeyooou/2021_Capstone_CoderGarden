c4f02a9c259564151f1dc814a74383463244fc73
jaeyooouinthebuildin <jaehyen2@naver.com>
2021-04-01 17:04:48 +0900

    Add signin,signup files

---
+++ config/my_Settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+# 나중에 ignore 에 추가하기
